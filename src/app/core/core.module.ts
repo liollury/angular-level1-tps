77924141aa14b4b67847ef0af0fa7212745c3876
Valentin GOT <gotvalentin@gmail.com>
2019-10-23 16:01:48 +0200

    chore(sidenav): add suggest button

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
+import { CoreServiceModule } from './core-service.module';
 import { HeaderComponent } from './header/header.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
-import { CoreServiceModule } from './core-service.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     CoreServiceModule
   ],
   declarations: [
