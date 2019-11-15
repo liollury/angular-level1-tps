d60af1bf03f5815693d0656da229191e0302b57c
LE LAY Olivier <liollury@gmail.com>
2019-11-15 16:50:10 +0100

    chore(bam)

--- src/app/core/user/user.model.ts
+++
@@ -1,4 +0,0 @@
-export class UserModel {
-  firstName: string;
-  lastName: string;
-}
--- src/app/shared/comics/comic.model.ts
+++
@@ -1,8 +0,0 @@
-export class ComicModel {
-  public id: number;
-  public volumeId: number;
-  public name: string;
-  public date: string;
-  public imageUrl: string;
-  public description?: string;
-}
