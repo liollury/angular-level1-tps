c2250446db6dc5ac2cd5d3fbc992e1e66d8f9262
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2019-11-20 13:52:06 +0100

    update tp01

---
+++ src/app/model/comic.model.ts
@@ -0,0 +1,8 @@
+export class ComicModel {
+  public id: number;
+  public volumeId: number;
+  public name: string;
+  public date: string;
+  public imageUrl?: string;
+  public description?: string;
+}
