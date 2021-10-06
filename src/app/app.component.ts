0016e3d9341e7ebc2648f77ac16bf567fd2e3c6c
Valentin Got <gotvalentin@gmail.com>
2021-10-06 14:41:32 +0200

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,18 @@
 import { Component } from '@angular/core';
-
 import { ComicModel } from './model/comic.model';
 
 @Component({
-  selector   : 'comics-root',
+  selector: 'comics-root',
   templateUrl: './app.component.html',
-  styleUrls  : ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
   comic: ComicModel = {
-    date       : '1990-11-14',
-    id         : 197814,
-    name       : 'Les gouttes de dieu',
-    volumeId   : 31638,
-    imageUrl   : '1812641-couv_212.jpg',
+    date: '1990-11-14',
+    id: 197814,
+    name: 'Les gouttes de dieu',
+    volumeId: 31638,
+    imageUrl: '1812641-couv_212.jpg',
     description: `After the death of Nerio Winch, Largos adopted fatherm a conspiriacy is set in motion to
       eliminate Largo and prevent him from taking over the empire.Largo is incarcerated in a prison in istanbul
       after being framed for murder and meets the lovable thief Simon Ovronnaz and they become friends.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports     : [
+  imports: [
     BrowserModule
   ],
   declarations: [
     AppComponent
   ],
-  bootstrap   : [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule {
-}
+export class AppModule { }
